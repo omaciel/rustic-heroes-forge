8600a44fa91321bec5aa30b19b8098c0a0210167
Og B. Maciel <omaciel@ogmaciel.com>
2023-05-24 21:53:40 -0400

    Fixes E0277 for enum Class output.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
         Ok(character) => {
             println!("Character created:");
             println!("Race: {:?}", character.race);
-            println!("Class: {}", character.class);
+            println!("Class: {:?}", character.class);
             println!("Ability Scores: {:?}", character.ability_scores);
             println!("Personality Traits: {:?}", character.personality_traits);
         },
