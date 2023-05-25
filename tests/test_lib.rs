c515a2204bd938f03492875b78819a6152e5d611
Og B. Maciel <omaciel@ogmaciel.com>
2023-05-24 21:42:06 -0400

    Removed integration tests.

--- tests/test_lib.rs
+++
@@ -1,8 +0,0 @@
-use rustic_heroes_forge::Character;
-use rustic_heroes_forge::Race;
-
-#[test]
-fn test_default_character() {
-    let character = Character::new();
-    assert!(matches!(character.race, Race::Human))
-}
