2ea913dc984725c9e5574aefa45c5134d85e52a5
Og B. Maciel <omaciel@ogmaciel.com>
2023-05-24 21:42:33 -0400

    Use enumeration for character class type.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 // Enumerations for a Character
 
+use std::error::Error;
+use std::io;
+
+
 #[derive(Debug)]
 pub enum Race {
     Dwarf,
@@ -7,11 +11,18 @@ pub enum Race {
     Human,
 }
 
+#[derive(Debug)]
+pub enum Class {
+    Rogue,
+    Spellcaster,
+    Warrior,
+}
+
 
 // Struct to hold character traits
 pub struct Character {
     pub race: Race,
-    pub class: String,
+    pub class: Class,
     pub ability_scores: Vec<i32>,
     pub personality_traits: Vec<String>,
 }
@@ -21,7 +32,7 @@ impl Character {
     pub fn new() -> Character {
         Character {
             race: Race::Human,
-            class: String::new(),
+            class: Class::Warrior,
             ability_scores: Vec::new(),
             personality_traits: Vec::new(),
         }
@@ -34,6 +45,60 @@ impl Default for Character {
     }
 }
 
+// Function to prompt the user for character traits
+pub fn run() -> Result<Character, Box<dyn Error>> {
+    let mut character = Character::new();
+
+    // Prompt for race
+    println!("Choose a race:");
+    println!("1. Human");
+    println!("2. Elf");
+    println!("3. Dwarf");
+    let mut race_choice = String::new();
+    io::stdin().read_line(&mut race_choice)?;
+    match race_choice.trim().parse() {
+        Ok(1) => character.race = Race::Human,
+        Ok(2) => character.race = Race::Elf,
+        Ok(3) => character.race = Race::Dwarf,
+        _ => println!("Invalid choice. Defaulting to Human."),
+    }
+
+    // Prompt for class
+    println!("Choose a class:");
+    println!("1. Warrior");
+    println!("2. Spellcaster");
+    println!("3. Rogue");
+    let mut class_choice = String::new();
+    io::stdin().read_line(&mut class_choice)?;
+    match class_choice.trim().parse() {
+        Ok(1) => character.class = Class::Warrior,
+        Ok(2) => character.class = Class::Spellcaster,
+        Ok(3) => character.class = Class::Rogue,
+        _ => println!("Invalid choice. Defaulting to Warrior."),
+    }
+
+    // Prompt for ability scores
+    println!("Enter ability scores (separated by spaces):");
+    let mut ability_scores = String::new();
+    io::stdin().read_line(&mut ability_scores)?;
+    let scores: Vec<i32> = ability_scores
+        .split_whitespace()
+        .map(|s| s.parse().unwrap_or(0))
+        .collect();
+    character.ability_scores = scores;
+
+    // Prompt for personality traits
+    println!("Enter personality traits (separated by commas):");
+    let mut personality_traits = String::new();
+    io::stdin().read_line(&mut personality_traits)?;
+    character.personality_traits = personality_traits
+        .split(',')
+        .map(|s| s.trim().to_string())
+        .collect();
+
+    Ok(character)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -41,6 +106,19 @@ mod tests {
     #[test]
     fn test_default_character() {
         let character = Character::new();
-        assert!(matches!(character.race, Race::Human))
+        assert!(matches!(character.race, Race::Human));
+        assert!(matches!(character.class, Class::Warrior));
+    }
+
+    #[test]
+    fn test_set_race() {
+        let character: Character = Character{
+            race: Race::Dwarf, 
+            class: Class::Spellcaster, 
+            ability_scores: Vec::new(), 
+            personality_traits: Vec::new() 
+        };
+        assert!(matches!(character.race, Race::Dwarf));
+        assert!(matches!(character.class, Class::Spellcaster));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,19 @@
-use std::io;
-use rustic_heroes_forge::Character;
-use rustic_heroes_forge::Race;
+use std::process;
+use rustic_heroes_forge::run;
 
-// Function to prompt the user for character traits
-fn prompt_character_creation() -> Character {
-    let mut character = Character::new();
-
-    // Prompt for race
-    println!("Choose a race:");
-    println!("1. Human");
-    println!("2. Elf");
-    println!("3. Dwarf");
-    let mut race_choice = String::new();
-    io::stdin().read_line(&mut race_choice).expect("Failed to read line");
-    match race_choice.trim().parse() {
-        Ok(1) => character.race = Race::Human,
-        Ok(2) => character.race = Race::Elf,
-        Ok(3) => character.race = Race::Dwarf,
-        _ => println!("Invalid choice. Defaulting to Human."),
-    }
-
-    // Prompt for class
-    println!("Choose a class:");
-    println!("1. Warrior");
-    println!("2. Spellcaster");
-    println!("3. Rogue");
-    let mut class_choice = String::new();
-    io::stdin().read_line(&mut class_choice).expect("Failed to read line");
-    match class_choice.trim().parse() {
-        Ok(1) => character.class = String::from("Warrior"),
-        Ok(2) => character.class = String::from("Spellcaster"),
-        Ok(3) => character.class = String::from("Rogue"),
-        _ => println!("Invalid choice. Defaulting to Warrior."),
-    }
-
-    // Prompt for ability scores
-    println!("Enter ability scores (separated by spaces):");
-    let mut ability_scores = String::new();
-    io::stdin().read_line(&mut ability_scores).expect("Failed to read line");
-    let scores: Vec<i32> = ability_scores
-        .split_whitespace()
-        .map(|s| s.parse().unwrap_or(0))
-        .collect();
-    character.ability_scores = scores;
-
-    // Prompt for personality traits
-    println!("Enter personality traits (separated by commas):");
-    let mut personality_traits = String::new();
-    io::stdin().read_line(&mut personality_traits).expect("Failed to read line");
-    character.personality_traits = personality_traits
-        .split(',')
-        .map(|s| s.trim().to_string())
-        .collect();
-
-    character
-}
 
 fn main() {
-    let character = prompt_character_creation();
-    println!("Character created:");
-    println!("Race: {:?}", character.race);
-    println!("Class: {}", character.class);
-    println!("Ability Scores: {:?}", character.ability_scores);
-    println!("Personality Traits: {:?}", character.personality_traits);
+    match run() {
+        Ok(character) => {
+            println!("Character created:");
+            println!("Race: {:?}", character.race);
+            println!("Class: {}", character.class);
+            println!("Ability Scores: {:?}", character.ability_scores);
+            println!("Personality Traits: {:?}", character.personality_traits);
+        },
+        Err(e) => {
+            eprintln!("Application error: {e}");
+            process::exit(1);
+        }
+    }
 }
